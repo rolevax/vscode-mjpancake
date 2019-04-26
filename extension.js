a2a734fc026568ad10af547e678da897f04bb81a
rolevax <rolevax@gmail.com>
2019-04-26 16:09:31 +0800

    fuck callback hell

--- extension.js
+++ extension.js
@@ -14,40 +14,56 @@ end`
 function activate(context) {
 	console.log('extension "mjpancake" is now active');
 
-	let disposable = vscode.commands.registerCommand('mjpancake.newGirl', function () {
-		vscode.window.showSaveDialog({
-			defaultUri: vscode.Uri.file("untitled"),
-			saveLabel: "Create Girl",
-			filters: {
-				"Pancake Mahjong Girl File": [ "girl.json" ]
-			}
-		}).then(uri => {
-			let jsonPath = uri.fsPath;
-			let luaPath = jsonPath.substr(0, jsonPath.length - 4) + "lua";
-			// TODO if already exists, go to 'open girl' hook
-			vscode.workspace.openTextDocument(vscode.Uri.parse("untitled:" + jsonPath)).then(jsonDoc => {
-				vscode.workspace.openTextDocument(vscode.Uri.parse("untitled:" + luaPath)).then(luaDoc => {
-					vscode.window.showTextDocument(jsonDoc, vscode.ViewColumn.Active, false).then(jsonEditor => {
-						jsonEditor.edit(editBuilder => {
-							editBuilder.insert(new vscode.Position(0, 0), '{"name":"New Girl"}');
-						});
-						vscode.window.showTextDocument(luaDoc, vscode.ViewColumn.Active, true).then(luaEditor => {
-							vscode.commands.executeCommand("workbench.action.previousEditorInGroup");
-							luaEditor.edit(editBuilder => {
-								editBuilder.insert(new vscode.Position(0, 0), sampleLuaCode);
-							})
-						})
-					})
-				})
-			});
-		})
-	});
+	let disposable = vscode.commands.registerCommand('mjpancake.newGirl', openNewGirl);
 
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
 function deactivate() {}
+
+function openNewGirl() {
+	vscode.window.showSaveDialog({
+		defaultUri: vscode.Uri.file("untitled"),
+		saveLabel: "Create Girl",
+		filters: {
+			"Pancake Mahjong Girl File": [ "girl.json" ]
+		}
+	}).then(openNewGirlFrom);
+}
+
+function openNewGirlFrom(jsonUri) {
+	let luaUri = vscode.Uri.file(jsonUri.fsPath.substr(0, jsonUri.fsPath.length - 4) + "lua")
+	let edits = new vscode.WorkspaceEdit();
+	edits.createFile(jsonUri, { ignoreIfExists: true });
+	edits.createFile(luaUri, { ignoreIfExists: true });
+	vscode.workspace.applyEdit(edits).then(ok => {
+		if (!ok) {
+			vscode.window.showErrorMessage("Failed to create new girl files");
+			return;
+		}
+
+		openNewGirlFiles(jsonUri, luaUri);
+	});
+}
+
+function openNewGirlFiles(jsonUri, luaUri) {
+	vscode.workspace.openTextDocument(jsonUri).then(jsonDoc => {
+		vscode.workspace.openTextDocument(luaUri).then(luaDoc => {
+			vscode.window.showTextDocument(jsonDoc, vscode.ViewColumn.Active, false).then(jsonEditor => {
+				jsonEditor.edit(editBuilder => {
+					editBuilder.insert(new vscode.Position(0, 0), '{"name":"New Girl"}');
+				});
+				vscode.window.showTextDocument(luaDoc, vscode.ViewColumn.Active, true).then(luaEditor => {
+					vscode.commands.executeCommand("workbench.action.previousEditorInGroup");
+					luaEditor.edit(editBuilder => {
+						editBuilder.insert(new vscode.Position(0, 0), sampleLuaCode);
+					})
+				})
+			})
+		})
+	});
+}
 
 module.exports = {
 	activate,
