72615ce6b8ca7dfcc440cad2873743eba4c48e16
rolevax <rolevax@gmail.com>
2019-04-26 18:11:34 +0800

    add webview test code

--- extension.js
+++ extension.js
@@ -1,4 +1,5 @@
 const vscode = require('vscode');
+const path = require('path');
 
 const sampleLuaCode = `function ondraw()
   if who ~= self or rinshan then
@@ -12,60 +13,92 @@ end`
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	console.log('extension "mjpancake" is now active');
+    console.log('extension "mjpancake" is now active');
 
-	let disposable = vscode.commands.registerCommand('mjpancake.newGirl', openNewGirl);
+    // let disposable = vscode.commands.registerCommand('mjpancake.newGirl', openNewGirl);
+    let disposable = vscode.commands.registerCommand('mjpancake.newGirl', () => {
+        openGirlJsonEditor(vscode.Uri.file("~/code/lua/first.girl.json"));
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
 function deactivate() {}
 
 function openNewGirl() {
-	vscode.window.showSaveDialog({
-		defaultUri: vscode.Uri.file("untitled"),
-		saveLabel: "Create Girl",
-		filters: {
-			"Pancake Mahjong Girl File": [ "girl.json" ]
-		}
-	}).then(openNewGirlFrom);
+    vscode.window.showSaveDialog({
+        defaultUri: vscode.Uri.file("untitled"),
+        saveLabel: "Create Girl",
+        filters: {
+            "Pancake Mahjong Girl File": [ "girl.json" ]
+        }
+    }).then(openNewGirlFrom);
 }
 
 function openNewGirlFrom(jsonUri) {
-	let luaUri = vscode.Uri.file(jsonUri.fsPath.substr(0, jsonUri.fsPath.length - 4) + "lua")
-	let edits = new vscode.WorkspaceEdit();
-	edits.createFile(jsonUri, { ignoreIfExists: true });
-	edits.createFile(luaUri, { ignoreIfExists: true });
-	vscode.workspace.applyEdit(edits).then(ok => {
-		if (!ok) {
-			vscode.window.showErrorMessage("Failed to create new girl files");
-			return;
-		}
+    let luaUri = vscode.Uri.file(jsonUri.fsPath.substr(0, jsonUri.fsPath.length - 4) + "lua")
+    let edits = new vscode.WorkspaceEdit();
+    edits.createFile(jsonUri, { ignoreIfExists: true });
+    edits.createFile(luaUri, { ignoreIfExists: true });
+    vscode.workspace.applyEdit(edits).then(ok => {
+        if (!ok) {
+            vscode.window.showErrorMessage("Failed to create new girl files");
+            return;
+        }
 
-		openNewGirlFiles(jsonUri, luaUri);
-	});
+        openNewGirlFiles(jsonUri, luaUri);
+    });
 }
 
 function openNewGirlFiles(jsonUri, luaUri) {
-	vscode.workspace.openTextDocument(jsonUri).then(jsonDoc => {
-		vscode.workspace.openTextDocument(luaUri).then(luaDoc => {
-			vscode.window.showTextDocument(jsonDoc, vscode.ViewColumn.Active, false).then(jsonEditor => {
-				jsonEditor.edit(editBuilder => {
-					editBuilder.insert(new vscode.Position(0, 0), '{"name":"New Girl"}');
-				});
-				vscode.window.showTextDocument(luaDoc, vscode.ViewColumn.Active, true).then(luaEditor => {
-					vscode.commands.executeCommand("workbench.action.previousEditorInGroup");
-					luaEditor.edit(editBuilder => {
-						editBuilder.insert(new vscode.Position(0, 0), sampleLuaCode);
-					})
-				})
-			})
-		})
-	});
+    openGirlJsonEditor(jsonUri)
+
+    vscode.workspace.openTextDocument(luaUri).then(luaDoc => {
+        vscode.window.showTextDocument(luaDoc, vscode.ViewColumn.Active, true).then(luaEditor => {
+            vscode.commands.executeCommand("workbench.action.previousEditorInGroup"); // back to json tab
+            luaEditor.edit(editBuilder => {
+                editBuilder.insert(new vscode.Position(0, 0), sampleLuaCode);
+            })
+        })
+    });
+}
+
+function openGirlJsonEditor(jsonUri) {
+    const panel = vscode.window.createWebviewPanel(
+        'mjpancakeGirlJsonEditor',
+        path.basename(jsonUri.fsPath),
+        vscode.ViewColumn.Active,
+        {}
+    );
+    panel.webview.html = getGirlJsonEditorHtml();
+}
+
+function getGirlJsonEditorHtml() {
+    return `<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+</head>
+<body>
+  <a></a>
+  <form>
+    <table>
+      <tr><td>Name</td><td><input type="text" /></td>
+      <tr><td>Photo</td><td><input type="file"></td></tr>
+    </table>
+  </form>
+  <button>Save</button>
+  <button>Test with 3 doges</button>
+  <button>Export to Pancake</button>
+  <img src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" alt="Red dot" />
+</body>
+</html>`;
 }
 
 module.exports = {
-	activate,
-	deactivate
+    activate,
+    deactivate
 }
