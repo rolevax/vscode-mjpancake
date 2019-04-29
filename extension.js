4b664600a051dca812ec8a77e393778566adaddd
rolevax <rolevax@gmail.com>
2019-04-29 11:09:18 +0800

    split html to file

--- extension.js
+++ extension.js
@@ -8,13 +8,18 @@ const sampleLuaCode = `function ondraw()
   end
 
   print("好像能用了，耶！")
-end`
+end`;
+
+let girlJsonEditorHtml = "";
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
     console.log('extension "mjpancake" is now active');
+
+    const filePath = vscode.Uri.file(path.join(context.extensionPath, 'girl-json-editor.html'));
+    girlJsonEditorHtml = fs.readFileSync(filePath.fsPath, 'utf8');
 
     // let disposable = vscode.commands.registerCommand('mjpancake.newGirl', openNewGirl);
     let disposable = vscode.commands.registerCommand('mjpancake.newGirl', () => {
@@ -74,7 +79,7 @@ function openGirlJsonEditor(jsonUri) {
             enableScripts: true
         }
     );
-    panel.webview.html = getGirlJsonEditorHtml();
+    panel.webview.html = girlJsonEditorHtml;
 
     vscode.workspace.openTextDocument(jsonUri).then(jsonDoc => {
         let jsonText = jsonDoc.getText();
@@ -92,100 +97,6 @@ function openGirlJsonEditor(jsonUri) {
             vscode.window.showInformationMessage("Girl file saved");
         }); 
     });
-}
-
-function getGirlJsonEditorHtml() {
-    return `<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <style>
-        img {
-            min-width: 300px;
-            min-height: 500px;
-            max-width: 300px;
-            max-height: 500px;
-        }
-    </style>
-</head>
-<body>
-    <a id="fuck"></a>
-    <form id="form">
-        <table>
-            <tr>
-                <td>Name</td>
-                <td><input type="text" name="name" /></td>
-            </tr>
-            <tr>
-                <td>Photo</td>
-                <td><input type="file" name="photo" accept="image/png, image/jpeg"></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><img id="preview"></img></td>
-            </tr>
-            <tr>
-                <td><button id="buttonSave">Save</button></td>
-            </tr>
-            <tr>
-                <td><button>3 Doge Match</button></td>
-            </tr>
-            <tr>
-                <td><button>Export to Pancake</button></td>
-            </tr>
-        </table>
-    </form>
-
-    <script>
-        const form = document.getElementById("form");
-        const fuck = document.getElementById("fuck");
-        const preview = document.getElementById("preview");
-        const buttonSave = document.getElementById("buttonSave");
-        const vscode = acquireVsCodeApi();
-
-        window.addEventListener('message', event => {
-            const message = event.data; // The JSON data our extension sent
-            form.name.value = message.name;
-            preview.base64 = message.photoBase64;
-            preview.src = "data:image/png;base64," + preview.base64;
-        });
-
-        form.photo.addEventListener('change', event => {
-            if (form.photo.files.length > 0) {
-                let image = form.photo.files[0];
-                preview.src = window.URL.createObjectURL(image);
-            }
-        });
-
-        preview.onload = () => {
-            let canvas = document.createElement("canvas");
-            canvas.width = preview.width;
-            canvas.height = preview.height;
-            let ctx = canvas.getContext("2d");
-            ctx.drawImage(preview, 0, 0, preview.width, preview.height);
-            let dataURL = canvas.toDataURL();
-            preview.base64 = dataURL.split(",")[1];
-            canvas = null;
-        };
-
-        buttonSave.onclick = () => {
-            let girlJson = {
-                name: form.name.value,
-                photoBase64: preview.base64
-            };
-            if (!girlJson.name) {
-                girlJson.name = "";
-            }
-            if (!girlJson.photoBase64) {
-                girlJson.photoBase64 = "";
-            }
-            vscode.postMessage(girlJson);
-        };
-    </script>
-</body>
-</html>`;
 }
 
 module.exports = {
